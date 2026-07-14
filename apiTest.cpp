ed7e8170fb8c7d8d526728eddc1bd48a47065a86
adamneula <adamneula@gmail.com>
2026-07-14 15:03:41 -0700

    Successfully pulls price info from blpapi

--- apiTest.cpp
+++ apiTest.cpp
@@ -11,7 +11,7 @@ void wait(int time) {
 
 double getPrice(std::string ticker) {
     SessionOptions options;
-
+//Connect to bloomberg on machine
     options.setServerHost("localhost");
     options.setServerPort(8194);
     Session session(options);
@@ -31,19 +31,45 @@ double getPrice(std::string ticker) {
         return -1;
     }
 
+    //Retrieve reference data
     Service refDataService = session.getService("//blp/refdata");
     std::cout << "Reference Data Service has been successfully opened!" << std::endl;
 
+    //Build request in abstract data type
     Request request = refDataService.createRequest("ReferenceDataRequest");
     request.append("securities", ticker.c_str());
     request.append("fields", "PX_LAST");
     std::cout << "Request form build cleanly for ticker: " << ticker << std::endl;
-    
+
+    //Send request
+    session.sendRequest(request);
+
+    double price = -1.0;
+    bool done = false;
+
+    while (!done) {
+        //pauses execution to await package arrival
+        Event event = session.nextEvent();
+
+        if (event.eventType() == Event::PARTIAL_RESPONSE || event.eventType() == Event::RESPONSE) {
+            MessageIterator msgIter(event);
+            while (msgIter.next()) {
+                Message msg = msgIter.message();
+                msg.print(std::cout);
+            }
+        }
+        if (event.eventType() == Event::RESPONSE) {
+            done = true;
+        }
+    }
+
+    session.stop();
     wait(5);
     return 0.0;
 }
 
 int main() {
-    std::cout << "Here is the most recent price of AAPL: " << getPrice(std::string("AAPL")) << std::endl;
+    std::cout << "Here is the most recent price of AAPL: " << getPrice(std::string("AAPL US EQUITY")) << std::endl;
+    wait(20);
     return 0;
 }
